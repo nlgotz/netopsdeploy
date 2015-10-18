63c91c18d942f9da83097a40538064915420ac9a
Nathan Gotz <nlgotz+github@gotzweb.com>
2015-10-17 19:57:17 -0500

    Added ability to create a and ptr records in infoblox

--- netopsdeploy/cli/plugins/infoblox.py
+++ netopsdeploy/cli/plugins/infoblox.py
@@ -2,14 +2,107 @@
 
 from cement.core.controller import CementBaseController, expose
 from cement.core import handler, hook
+import infoblox
 
+"""
+Extends the Infoblox API found here:
+https://github.com/Infoblox-Development/Infoblox-API-Python
+"""
+class InfobloxSubclass(infoblox.Infoblox):
+    def create_a_record(self, address, fqdn):
+        """ Implements IBA REST API call to create IBA a record
+        :param address: IP v4 address or NET v4 address in CIDR format to get next_available_ip from
+    	:param fqdn: hostname in FQDN
+        """
+        rest_url = 'https://' + self.iba_host + '/wapi/v' + self.iba_wapi_version + '/record:a'
+        payload = '{"ipv4addr": "' + address + '","name": "' + fqdn + '","view": "' + self.iba_dns_view + '"}'
+        try:
+            r = requests.post(url=rest_url, auth=(self.iba_user, self.iba_password), verify=self.iba_verify_ssl, data=payload)
+            r_json = r.json()
+            if r.status_code == 200 or r.status_code == 201:
+                return
+            else:
+                if 'text' in r_json:
+                    raise InfobloxGeneralException(r_json['text'])
+                else:
+                    r.raise_for_status()
+        except ValueError:
+            raise Exception(r)
+        except Exception:
+            raise
+
+    def create_ptr_record(self, address, fqdn):
+        """ Implements IBA REST API call to create IBA ptr record
+        :param address: IP v4 address or NET v4 address in CIDR format to get next_available_ip from
+	    :param fqdn: hostname in FQDN
+        """
+        rest_url = 'https://' + self.iba_host + '/wapi/v' + self.iba_wapi_version + '/record:ptr'
+        payload = '{"ipv4addr": "' + address + '","ptrdname": "' + fqdn + '","view": "' + self.iba_dns_view + '"}'
+        try:
+            r = requests.post(url=rest_url, auth=(self.iba_user, self.iba_password), verify=self.iba_verify_ssl, data=payload)
+            r_json = r.json()
+            if r.status_code == 200 or r.status_code == 201:
+                return
+            else:
+                if 'text' in r_json:
+                    raise InfobloxGeneralException(r_json['text'])
+                else:
+                    r.raise_for_status()
+        except ValueError:
+            raise Exception(r)
+        except Exception:
+            raise
+
+    def create_device_record(self, address, fqdn):
+        """ Implements IBA REST API call to create IBA a and ptr record
+        :param address: IP v4 address or NET v4 address in CIDR format to get next_available_ip from
+	    :param fqdn: hostname in FQDN
+        """
+        try:
+            self.create_a_record(address, fqdn)
+        except ValueError:
+            raise Exception(r)
+        except Exception:
+            raise
+        try:
+            self.create_ptr_record(address, fqdn)
+        except ValueError:
+            raise Exception(r)
+        except Exception:
+            raise
+
+"""
+Adds device A and PTR records. This could probably be extended further if you
+record other information in Infoblox
+"""
 def infoblox_plugin_hook(app):
     # do something with the ``app`` object here.
-    print "Infoblox:"
-    print("Server   : %s" % app.config.get('infoblox', 'ib_server'))
-    print("Username : %s" % app.config.get('infoblox', 'ib_username'))
-    print("Password : %s\r\n" % app.config.get('infoblox', 'ib_password'))
+    IP_input = app.pargs.ip.strip()
+    server = app.config.get('infoblox', 'ib_server')
+    username = app.config.get('infoblox', 'ib_username')
+    password = app.config.get('infoblox', 'ib_password')
+    version = app.config.get('infoblox', 'ib_version')
 
+    hostname = app.pargs.hostname
+    domain = app.config.get('netopsdeploy', 'domain')
+    # Create the fqdn for the host
+    fqdn = hostname + "." + domain
+
+    # Print basic information
+    print "Infoblox:"
+    print("Server   : %s" % server)
+    print("Version  : %s\r\n" % version)
+
+    # Create Connection to Infoblox
+    iba_api = InfobloxSubclass(server, username, password, version, 'default', 'default')
+
+    try:
+        ip = iba_api.create_device_record(IP_input, fqdn)
+        print ip
+    except Exception as e:
+        print e
+
+    print("Created A and PTR records for:   %s" % fqdn)
     pass
 
 class InfobloxPluginController(CementBaseController):
@@ -30,11 +123,11 @@ class InfobloxPluginController(CementBaseController):
         # ``$ netopsdeploy example --help``
         arguments = [
             (
-                ['-f', '--foo'],
-                dict(
-                    help='Notorious foobar option',
-                    action='store',
-                    )
+                #['-f', '--foo'],
+                #dict(
+                #    help='Notorious foobar option',
+                #    action='store',
+                #    )
             )
         ]
 
