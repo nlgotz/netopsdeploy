35cb0478653c89671a73935ed5684aacdf8f33ef
Nate Gotz <nathan.gotz@we-energies.com>
2015-10-20 13:36:56 -0500

    Added requirement to put in flags

--- netopsdeploy/cli/controllers/base.py
+++ netopsdeploy/cli/controllers/base.py
@@ -21,27 +21,10 @@ class NetopsDeployBaseController(CementBaseController):
             (['-d', '--domain'],
              dict(help='DNS domain of the device', dest='domain', action='store',
                   metavar='TEXT') ),
-            (['-T', '--test'],
-             dict(help='Test the variables. Does not actually insert data', dest='test',
-                  action='store_true',) ),
-            (['--solarwinds-server'],
-             dict(help='Solarwinds Server Name', dest='sw_server', action='store',
-                  metavar='TEXT') ),
-            (['--solarwinds-username'],
-             dict(help='Solarwinds Username', dest='sw_username', action='store',
-                  metavar='TEXT') ),
-            (['--solarwinds-password'],
-             dict(help='Solarwinds Password', dest='sw_password', action='store',
-                  metavar='TEXT') ),
-            (['--infoblox-server'],
-             dict(help='Infoblox Server Name', dest='ib_server', action='store',
-                  metavar='TEXT') ),
-            (['--infoblox-username'],
-             dict(help='Infoblox Username', dest='ib_username', action='store',
-                  metavar='TEXT') ),
-            (['--infoblox-password'],
-             dict(help='Infoblox Password', dest='ib_password', action='store',
-                  metavar='TEXT') ),
+            (['-ib', '--infoblox'],
+             dict(help='Add device to Infoblox', dest='infoblox', action='store_true')),
+            (['-s', '--solarwinds'],
+             dict(help='Add device to Solar Winds', dest='solarwinds', action='store_true')),
             ]
 
     @expose(hide=False)
@@ -61,17 +44,35 @@ class NetopsDeployBaseController(CementBaseController):
                 return("Test successful")
                 pass
 
-            # run infoblox
-            import netopsdeploy.cli.plugins.ib as ib
-            ib.ib_plugin_hook(self.app)
-
-            # run solarwinds
-            import netopsdeploy.cli.plugins.solarwinds as solarwinds
-            solarwinds.solarwinds_plugin_hook(self.app)
+            if(self.app.pargs.infoblox):
+                # run infoblox
+                import netopsdeploy.cli.plugins.ib as ib
+                ib.ib_plugin_hook(self.app)
+            
+            if(self.app.pargs.solarwinds):
+                # run solarwinds
+                import netopsdeploy.cli.plugins.solarwinds as solarwinds
+                solarwinds.solarwinds_plugin_hook(self.app)
 
         else:
             raise Exception('No IP or hostname supplied')
+    
+    @expose(hide=False)
+    def test(self):
+        # setup variables variables
+        test        = self.app.pargs.test
+        hostname    = self.app.pargs.hostname
+        ip          = self.app.pargs.ip
+        domain      = self.app.config.get('netopsdeploy', 'domain')
 
+
+
+        # make sure that an IP and hostname are supplied
+        if(ip and hostname):
+            NetopsDeployBaseController.printConfiguration(self, hostname, ip, domain, test)
+            return("Test successful")
+        else:
+            raise Exception('No IP or hostname supplied')
 
 
     @expose(hide=True)
